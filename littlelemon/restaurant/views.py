f841f1f5fbebb02e406e0c783a1fd25b4905d139
Oisin <oisin.leonard@gmail.com>
2025-05-18 11:53:42 +0100

    Configured basic url

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+#from .models import Menu
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html", {})
+
+#def menu(request):
+#    menu_data = Menu.objects.all()
+#    main_data = {"menu": menu_data}
+#    return render(request, "menu.html", {"menu": main_data})
