6db7d23c0b16d8a09fd7f8466e3129250873dfa4
Oisin <oisin.leonard@gmail.com>
2025-05-19 08:37:56 +0100

    Added api and authentication

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -19,13 +19,19 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from restaurant import views
+from rest_framework.authtoken.views import obtain_auth_token
+from restaurant.views import index
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', index),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/menu/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth', obtain_auth_token),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,7 +5,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from .models import Menu, Booking
-from .serializers import MenuItemSerializer, BookingSerializer
+from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
 
 def index(request):
     return render(request, "index.html", {})
@@ -21,3 +24,14 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.RetrieveUpdate
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated]
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
