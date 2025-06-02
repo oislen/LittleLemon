e06d45bad66e84197d8b649149779e4910149471
Oisin <oisin.leonard@gmail.com>
2025-06-02 19:58:20 +0100

    Moved menuitem and category to restaurant model

--- littlelemon/api/models.py
+++ littlelemon/api/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-class Category(models.Model):
-    slug = models.SlugField()
-    title = models.CharField(max_length=255, db_index=True)
-
-
-class MenuItem(models.Model):
-    title = models.CharField(max_length=255, db_index=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
-    featured = models.BooleanField(db_index=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-
+from restaurant.models import MenuItem
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -21,7 +10,6 @@ class Cart(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     class Meta:
         unique_together = ('menuitem', 'user')
-
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
