020ed5db714d3b26a83546569c7e0dd6357e016b
Oisin <oisin.leonard@gmail.com>
2025-06-03 08:51:28 +0100

    deprecated registered models for api

--- littlelemon/api/admin.py
+++ littlelemon/api/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Cart, Order, OrderItem
 
 # Register your models here.
-admin.site.register(Cart)
-admin.site.register(Order)
-admin.site.register(OrderItem)
