7609c5ebf94fc511be8ebd59bcdea97c015834f0
Oisin <oisin.leonard@gmail.com>
2025-06-03 08:47:08 +0100

    Created python script for batch importing inital data

---
+++ littlelemon/restaurant/import_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from django.contrib.auth.models import User
+from restaurant.models import Category, MenuItem, Booking
+
+def run():
+    # read csv file and iterate through the data and create model instances
+    menu_item_fpath = "restaurant/static/data/menu_items.csv"
+    menu_item_data = pd.read_csv(menu_item_fpath, encoding="utf-8")
+    for index, row in menu_item_data.iterrows():
+        # create or get the category instance
+        category, created = Category.objects.get_or_create(
+            title=row["category"],
+            defaults={"slug": row["category"].lower().replace(" ", "-")}
+        )
+        # create the instance
+        menuitem, created = MenuItem.objects.update_or_create(
+            name=row["name"],
+            price=row["price"],
+            quantity=row["quantity"],
+            description=row["description"],
+            featured=row["featured"],
+            category=category
+        )
+
+    # read csv file and iterate through the data and create model instances
+    booking_fpath = "restaurant/static/data/bookings.csv"
+    booking_data = pd.read_csv(booking_fpath, encoding="utf-8")
+    for index, row in booking_data.iterrows():
+        # create the instance
+        booking = Booking.objects.update_or_create(
+            first_name=row["first_name"],
+            last_name=row["last_name"],
+            guest_number=row["guest_number"],
+            comment=row["comment"]
+        )
