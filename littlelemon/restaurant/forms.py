4b3739a1322aa06136e0f8749a573b3ad47f792d
Oisin <oisin.leonard@gmail.com>
2025-06-03 20:45:19 +0100

    Added date time widget for date time field

--- littlelemon/restaurant/forms.py
+++ littlelemon/restaurant/forms.py
@@ -1,9 +1,14 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateTimeInput
 from .models import Booking
+from django.contrib.admin.widgets import AdminDateWidget
 
 
 # Code added for loading form data on the Booking page
 class BookingForm(ModelForm):
+    input_type ='datetime-local'
     class Meta:
         model = Booking
         fields = "__all__"
+        widgets = {
+            'date_time': DateTimeInput(attrs={'type': 'datetime-local'})
+        }
