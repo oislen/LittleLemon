3fad2563a3814968c9790ad1ee9a4b63a7735757
Oisin <oisin.leonard@gmail.com>
2025-06-03 08:51:05 +0100

    Registering models used for rest api

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Booking
 from .models import MenuItem
 from .models import Category
+from .models import Cart
+from .models import Order
+from .models import OrderItem
 
 # register models
 admin.site.register(MenuItem)
 admin.site.register(Booking)
 admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
