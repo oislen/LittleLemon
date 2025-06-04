b503c27c6d7f0f497007c41472998bb079034304
Oisin <oisin.leonard@gmail.com>
2025-06-04 15:09:55 +0100

    Modified data time formating

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -107,13 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
-USE_TZ = True
-
+USE_TZ=False
+DATETIME_FORMAT="%Y-%m-%d %H:%M:%S"
+L10N=False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
