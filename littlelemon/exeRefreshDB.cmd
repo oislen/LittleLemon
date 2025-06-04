1148b44ae6e840620524622baaa54cd9761690a9
Oisin <oisin.leonard@gmail.com>
2025-06-04 07:35:59 +0100

    Added windows .cmd file to refresh web app database data

---
+++ littlelemon/exeRefreshDB.cmd
@@ -0,0 +1,10 @@
+call del db.sqlite3
+call del restaurant\migrations /s /q
+:: make db migrations and import data
+call python manage.py makemigrations restaurant
+call python manage.py migrate
+call python manage.py runscript restaurant.import_data
+:: create admin super user
+call python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+:: run web-app
+call python manage.py runserver
