2ab25258aba49ef0d9cd7c0babd480fa3f8c969a
Oisin <oisin.leonard@gmail.com>
2025-06-04 15:09:32 +0100

    Added verbose output for superuser creation. Calling tests as part of refresh process

--- littlelemon/exeRefreshDB.cmd
+++ littlelemon/exeRefreshDB.cmd
@@ -5,8 +5,8 @@ call python manage.py makemigrations restaurant
 call python manage.py migrate
 call python manage.py runscript restaurant.import_data
 :: create admin super user
-call python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+call python manage.py shell -v 3 -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 :: run django app test
-:: python manage.py test
+call python manage.py test
 :: run web-app
 call python manage.py runserver
