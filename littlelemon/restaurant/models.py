9805b6be21ecd3c57543f83ea1b32f107c719b09
Oisin <oisin.leonard@gmail.com>
2025-06-04 10:02:09 +0100

    Set booking comment blank to True

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -21,7 +21,7 @@ class Booking(models.Model):
    last_name = models.CharField(max_length=200)
    guest_number = models.PositiveIntegerField()
    date_time = models.DateTimeField()
-   comment = models.CharField(max_length=1000, default=None, null=True)
+   comment = models.CharField(max_length=1000, default=None, null=True, blank=True)
 
    def __str__(self):
       return self.first_name + ' ' + self.last_name
