6ef93eb4e353ab765969935d81140607e278d46b
Oisin <oisin.leonard@gmail.com>
2025-06-05 20:10:13 +0100

    Changed admin user to superuser1

--- littlelemon/exeRefreshDB.cmd
+++ littlelemon/exeRefreshDB.cmd
@@ -5,7 +5,7 @@ call python manage.py makemigrations restaurant
 call python manage.py migrate
 call python manage.py runscript restaurant.import_data
 :: create admin super user
-call python manage.py shell -v 3 -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+call python manage.py shell -v 3 -c "from django.contrib.auth.models import User; User.objects.create_superuser('superuser1', 'superuser1@littlelemon.com', 'superuser1')"
 :: run django app test
 call python manage.py test
 :: run web-app
