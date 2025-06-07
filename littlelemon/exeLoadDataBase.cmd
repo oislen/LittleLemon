7cb864e87093285443d73cacc9fb24c0b67e246b
Oisin <oisin.leonard@gmail.com>
2025-06-07 13:34:00 +0100

    Dropping user creation from refreshing database

--- littlelemon/exeLoadDataBase.cmd
+++ littlelemon/exeLoadDataBase.cmd
@@ -4,8 +4,6 @@ call del restaurant\migrations /s /q
 call python manage.py makemigrations restaurant
 call python manage.py migrate
 call python manage.py runscript restaurant.import_data
-:: create admin super user
-call python manage.py shell -v 3 -c "from django.contrib.auth.models import User; User.objects.create_superuser('superuser1', 'superuser1@littlelemon.com', 'superuser1')"
 :: run django app test
 call python manage.py test
 :: run web-app
