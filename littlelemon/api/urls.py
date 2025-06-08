4b2fbd90b48304cf9fc3c57fda2948be8bf23961
Oisin <oisin.leonard@gmail.com>
2025-06-08 10:16:36 +0100

    Added url route for ursers

--- littlelemon/api/urls.py
+++ littlelemon/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, MenuItemViewSet, BookingViewSet, CustomerOrderViewSet,AssignUserToManagerGroup, AssignToDeliveryCrew, DeliveryCrewOrderView
+from .views import CategoryViewSet, MenuItemViewSet, BookingViewSet, CustomerOrderViewSet,AssignUserToManagerGroup, AssignToDeliveryCrew, DeliveryCrewOrderView, UserViewSet
 
 router = DefaultRouter()
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('menu-items', MenuItemViewSet, basename='menu-items')
 router.register('orders', CustomerOrderViewSet, basename='orders')
 router.register('bookings', BookingViewSet, basename='bookings')
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
